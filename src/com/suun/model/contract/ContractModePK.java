aee28483850ebdf64791a08f7bcaf7e86548b342
171381138 <171381138@qq.com>
2015-07-05 23:39:15 +0800

    联合主键

---
+++ src/com/suun/model/contract/ContractModePK.java
@@ -0,0 +1,68 @@
+package com.suun.model.contract;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ * 合同对应数据库表配置表  联合主键
+ * @author renlq
+ */
+@Embeddable
+public class ContractModePK implements Serializable{
+	private static final long serialVersionUID = 1L;
+	//合同编号
+	private String contractId;
+	//数据库表名称
+	private String tableName;
+	
+	@Column(length=30)
+	public String getContractId() {
+		return contractId;
+	}
+	public void setContractId(String contractId) {
+		this.contractId = contractId;
+	}
+	
+	@Column(length=30)
+	public String getTableName() {
+		return tableName;
+	}
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((contractId == null) ? 0 : contractId.hashCode());
+		result = prime * result
+				+ ((tableName == null) ? 0 : tableName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContractModePK other = (ContractModePK) obj;
+		if (contractId == null) {
+			if (other.contractId != null)
+				return false;
+		} else if (!contractId.equals(other.contractId))
+			return false;
+		if (tableName == null) {
+			if (other.tableName != null)
+				return false;
+		} else if (!tableName.equals(other.tableName))
+			return false;
+		return true;
+	}
+
+}
